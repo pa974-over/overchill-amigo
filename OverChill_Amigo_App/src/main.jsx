a7427997b3c5f5e05eeaaa527fcac48d3df3c461
pa974-over <pa97450@gmail.com>
2025-05-24 12:33:49 +0400

    Add files via upload

---
+++ OverChill_Amigo_App/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AssistantAmigoChill from "./AssistantAmigoChill";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AssistantAmigoChill />
+  </React.StrictMode>
+);
